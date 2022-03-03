0b6b563425b4d643cf9ce273293ef5941a3b4cd7
ZETAVI <249584502@qq.com>
2022-03-03 16:34:29 +0800

    项目框架_60%

---
+++ src/Method/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+    @Project -> File   :lidarScan -> __init__.py
+    @IDE    :PyCharm
+    @Author :Mr. LU
+    @Date   :2022-03-02 16:01
+    @Desc   :
+-------------------------------------------------
+   Change Activity:
+                   2022-03-02 16:01:
+-------------------------------------------------
+"""
+__author__ = 'bobi'
+
+from . import scanning
+from .scanning import *
+from . import clustering
+from .clustering import *
+from . import matching
+from .matching import *
---
+++ src/Method/clustering.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+    @Project -> File   :lidarScan -> clustering
+    @IDE    :PyCharm
+    @Author :Mr. LU
+    @Date   :2022-03-03 10:45
+    @Desc   :聚类线程
+            负责控制动态窗口与窗口点集聚类
+            维护返回动态窗口聚类集合objectQueue
+-------------------------------------------------
+   Change Activity:
+                   2022-03-03 10:45:
+-------------------------------------------------
+"""
+__author__ = 'bobi'
+
+import threading
+import queue
+
+
+class clustering:
+    """聚类方法类"""
+
+    # 构造函数
+    def __init__(self, dataQueue, flag, objectQueue):
+        self.dataQueue = dataQueue
+        self.objectQueue = objectQueue
+        self.flag = flag
+        # 启动线程
+        threading.Thread(target=self.startClustering, ).start()
+        print("hello")
+
+    # 启动聚类处理线程
+    def startClustering(self):
+        while self.flag[0]:
+            # 从队头取元素 等待时间不能错过1秒
+            # print(self.dataQueue.qsize())
+            pointsPeriod = self.dataQueue.get(block=True, timeout=1)
+
+            # todo 具体聚类方法
+            # object为元组 记录当前窗口内聚类完成的object
+            # object = ()
+            # 存入聚类对象集中，待后续拟合使用
+            # self.objectQueue.put(item=object, block=True, timeout=1)
+
+            # todo 去除处理完的点
+
+    # 根据初始点startPoint,动态窗口大小
+    def dynamicRegulation(self, startPoint):
+        windowSize = 0
+        # todo 具体对应关系
+
+        return windowSize

89442e53f31a81224d02b64a027d47ac47c6ddc0
Meryle <2668389239@qq.com>
2022-03-19 21:24:20 +0800

    第8次提交 跟新matching与goblefunc的结合

--- src/Method/globalFunc.py
+++ src/Method/globalFunc.py
@@ -18,6 +18,81 @@ import math
 from math import sqrt
 
 
+def judege(x, y):
+    tag = False
+    # 运用扫描窗口内边界点和原点的夹角角度初步排除
+    n = len(x)
+    if x[0] != 0 and x[n - 1] != 0:
+        x1 = x[0]
+        y1 = y[0]
+        x2 = 0
+        y2 = 0
+        x3 = x[n - 1]
+        y3 = y[n - 1]
+        a2 = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)
+        b2 = (x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2)
+        c2 = (x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3)
+        a = math.sqrt(a2)
+        b = math.sqrt(b2)
+        c = math.sqrt(c2)
+        pos = (a2 + b2 - c2) / (2 * a * b)  # 求出余弦值
+        angle = math.acos(pos)  # 余弦值装换为弧度值
+        realangle = math.degrees(angle)  # 弧度值转换为角度值
+        if realangle < 145 or realangle > 95:
+            tag = True
+
+    '''
+    # 添加即时对数据进行拟合并通过拟合得到的二次函数参数进行初步排除
+    z1 = np.polyfit(x, y, 2)  # 用2次多项式拟合，可改变多项式阶数；
+    p1 = np.poly1d(z1)  # 得到多项式系数，按照阶数从高到低排列
+    #print(p1)  # 显示多项式
+    Min = (4 * p1[0] * p1[2] - p1[1]**2) / (4 * p1[0])  #极点的y坐标 用于区分直角
+    if(p1[0] > 16 or (p1[2] > 0.09) or Min > 0.0066):#此处有待修正第二个判断条件
+        return tag;
+
+    y2 = [ 9.148*x[i]**2 + 0.005358*x[i] + 0.0002134 for i in range(len(x))]
+    sum = 0;
+    n = len(x);
+    for real_y, fit_y in zip(y, y2):
+        sum = (real_y - fit_y) ^ 2 + sum;
+    res = sum / n
+    if(res < 2.5*10**-5):
+        tag = True
+    '''
+    return tag
+
+
+def transform_matching(tempObj):
+    """
+    将以聚类的数据点进行坐标平移与坐标转换
+    angle为角度
+    distance为距离
+    角度和距离一一对应
+    :
+    """
+    # 找出距离最近的点及其标号
+    angle = []
+    distance = []
+    for point in tempObj:
+        angle.append(point.angle)
+        distance.append(point.range)
+    middle_index = distance.index(min(distance))
+    # 旋转
+    Min_angle = angle[middle_index]
+    # for point in tempObj:
+    #     point[0] = point[0] - tempObj[index][0] + 90
+
+    t_angle = [angle[i] - Min_angle + 90 for i in range(len(angle))]
+    # 极坐标转为笛卡尔坐标
+
+    x = [math.cos(math.radians(t_angle[i])) * distance[i] for i in range(len(t_angle))]
+    y = [math.sin(math.radians(t_angle[i])) * distance[i] for i in range(len(t_angle))]
+    # 向下平移
+    y2 = [y[i] - y[middle_index] for i in range(len(y))]
+    # 返回平移后的直角坐标和特征点在元组的序号
+    return x, y2, middle_index
+
+
 # 极坐标下两点距离(距离，角度)
 def distance(arg1, arg2):
     return sqrt(abs(pow(arg1.range, 2) + pow(arg2.range, 2) - 2 * arg1.range * arg2.range * math.cos(

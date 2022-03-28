080bff8dc94e56adcaf1d1e6c6b1d4929b039c99
ZETAVI <249584502@qq.com>
2022-03-28 15:29:47 +0800

    meryle 进行的变更

--- src/Method/globalFunc.py
+++ src/Method/globalFunc.py
@@ -18,48 +18,83 @@ import math
 from math import sqrt
 
 
-def judege(x, y):
-    tag = False
+def judege(x, y, middle_index):
+    tag = False  # 最终判断
+    tag_left = True  # 左边判断
+    tag_right = True  # 右边判断
     # 运用扫描窗口内边界点和原点的夹角角度初步排除
     n = len(x)
     if x[0] != 0 and x[n - 1] != 0:
-        x1 = x[0]
-        y1 = y[0]
-        x2 = 0
-        y2 = 0
-        x3 = x[n - 1]
-        y3 = y[n - 1]
-        a2 = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)
-        b2 = (x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2)
-        c2 = (x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3)
-        a = math.sqrt(a2)
-        b = math.sqrt(b2)
-        c = math.sqrt(c2)
-        pos = (a2 + b2 - c2) / (2 * a * b)  # 求出余弦值
-        angle = math.acos(pos)  # 余弦值装换为弧度值
-        realangle = math.degrees(angle)  # 弧度值转换为角度值
-        if realangle < 145 or realangle > 95:
-            tag = True
-
-    '''
-    # 添加即时对数据进行拟合并通过拟合得到的二次函数参数进行初步排除
-    z1 = np.polyfit(x, y, 2)  # 用2次多项式拟合，可改变多项式阶数；
-    p1 = np.poly1d(z1)  # 得到多项式系数，按照阶数从高到低排列
-    #print(p1)  # 显示多项式
-    Min = (4 * p1[0] * p1[2] - p1[1]**2) / (4 * p1[0])  #极点的y坐标 用于区分直角
-    if(p1[0] > 16 or (p1[2] > 0.09) or Min > 0.0066):#此处有待修正第二个判断条件
-        return tag;
-
-    y2 = [ 9.148*x[i]**2 + 0.005358*x[i] + 0.0002134 for i in range(len(x))]
-    sum = 0;
-    n = len(x);
-    for real_y, fit_y in zip(y, y2):
-        sum = (real_y - fit_y) ^ 2 + sum;
-    res = sum / n
-    if(res < 2.5*10**-5):
+        realangle = angle_calculate(x, y, 0, middle_index, n - 1)
+        # print("大夹角为:", realangle)
+        # if not (realangle < 145 and realangle > 92) or (realangle < 80):
+        # 当大夹角在80到92度之间,还有小于50度的角判定为直角
+        if realangle < 92 and realangle > 80:
+            #  if (realangle < 92 and realangle > 80) or realangle <= 50:            print("有直角")
+            print(x, y)
+            return False
+        #  当大夹角在大于165度为直线
+        if realangle > 165:
+            print("有直线")
+            print(x, y)
+            return False
         tag = True
-    '''
+        # else:
+        # 进一步判断
+        if n >= 5:
+            if middle_index > 1:
+                left_realangle = angle_calculate(x, y, 0, math.floor(middle_index / 2), middle_index)
+                # print("左夹角为:", left_realangle)
+                if left_realangle > 170:
+                    tag_left = False
+            if n - 1 - middle_index > 1:
+
+                right_realangle = angle_calculate(x, y, middle_index, math.floor((n - 1 + middle_index) / 2), n - 1)
+                # print("右夹角为:", right_realangle)
+                if right_realangle > 170:
+                    tag_right = False
+            # 当且仅当左右两边均像直线的情况下才认定为直角
+            tag = tag_left or tag_right
+        '''
+        # 添加即时对数据进行拟合并通过拟合得到的二次函数参数进行初步排除
+        z1 = np.polyfit(x, y, 2)  # 用2次多项式拟合，可改变多项式阶数；
+        p1 = np.poly1d(z1)  # 得到多项式系数，按照阶数从高到低排列
+        #print(p1)  # 显示多项式
+        Min = (4 * p1[0] * p1[2] - p1[1]**2) / (4 * p1[0])  #极点的y坐标 用于区分直角
+        if(p1[0] > 16 or (p1[2] > 0.09) or Min > 0.0066):#此处有待修正第二个判断条件
+            return tag;
+
+        y2 = [ 9.148*x[i]**2 + 0.005358*x[i] + 0.0002134 for i in range(len(x))]
+        sum = 0;
+        n = len(x);
+        for real_y, fit_y in zip(y, y2):
+            sum = (real_y - fit_y) ^ 2 + sum;
+        res = sum / n
+        if(res < 2.5*10**-5):
+            tag = True
+        '''
     return tag
+
+
+def angle_calculate(x, y, left, middle_index, right):
+    # print("左边界点：", left, "中间点：", middle_index, "右边界点：", right)
+    x1 = x[left]
+    y1 = y[left]
+    x2 = x[middle_index]
+    y2 = y[middle_index]
+    x3 = x[right]
+    y3 = y[right]
+    c2 = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)
+    a2 = (x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2)
+    b2 = (x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3)
+    a = math.sqrt(a2)
+    b = math.sqrt(b2)
+    c = math.sqrt(c2)
+    pos = (a2 + c2 - b2) / (2 * a * c)  # 求出余弦值
+    angle = math.acos(pos)  # 余弦值装换为弧度值
+    realangle = math.degrees(angle)  # 弧度值转换为角度值
+
+    return realangle
 
 
 def transform_matching(tempObj):
